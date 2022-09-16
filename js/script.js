66769030b9d818964b914f4b26ece0099bf17ea9
kajsujatu <michalmatwijow@gmail.com>
2022-09-16 20:20:46 +0200

    Objects in array and transfering variables to html document

--- js/script.js
+++ js/script.js
@@ -30,6 +30,7 @@ search.addEventListener('click', function() {
 	//searchInput.style.backgroundColor: "#DCDCDC";
 });
 
+
 //Array of all Comics
 const Comics = [
 	// Metronom
@@ -41,12 +42,11 @@ const Comics = [
 			statusOfAvailable: true,
 			filter: "Antologie/Integrale",
 			newComics: false
-		},
-		
+		},	
 	// Wiek Evy - XXe ciel.com
 		{
 			id: 2, 
-			title: "Wiek Evy - XXe ciel.comganie",
+			title: "Wiek Evy - XXe ciel.com",
 			writer: "Bernard Yslaire",
 			art: "Bernard Yslaire",
 			statusOfAvailable: true,
@@ -75,8 +75,79 @@ const Comics = [
 			filter: "Antologie/Integrale",
 			newComics: false
 		},
-
+	
+	// Brygada
+		{
+			id: 4, 
+			title: "Brygada",
+			writer: "Enrique Fernandez",
+			art: "Enrique Fernandez",
+			translator: "Piotr Beliński",
+			proofreader: "Damian Pietrzak",
+			description:
+				{
+				bold: "Historia od Enrique Fernandeza, jednego ze współczesnych mistrzów komiksu hiszpańskiego. Autor uważany jest za jednego z najlepszych kolorystów na świecie i to widać na każdej stronie tej historii. Nie tylko historyjki obrazkowe są jego domeną, pracuje również przy tworzeniu animacji.",
+				normal: "Krasnolud, weteran walk, kapitan Ivro został zwerbowany by dowodzić brygadą skazańców. Jego oddział walczy w trwającej od lat wojnie przeciwko czarnym elfom. Podczas jednej z bitew tajemnicza mgła rzuca ich na terytorium dziwnej, pełnej chaosu krainy. Tam będą musieli stoczyć kolejną wojnę, tym razem by przetrwać jako grupa. Elfy, wiedźmy, czarownice, ludzie, demony, smoki to wszystko znajdziecie w tej niezwykłej opowieści z gatunku dark fantasy. Prawie 100 stron unikalnego stylowo, wysmakowanego artystycznie komiksu."
+				},
+			img: 
+				{
+				coverRegular: "img/komiks/brygada/brygada_cover_regular-min.jpg",
+				coverAlternative1: "img/komiks/brygada/brygada_cover_alternative1.jpg",
+				inside1: "img/komiks/brygada/brygada_inside1.jpg",
+				inside2: "img/komiks/brygada/brygada_inside2.jpg",
+				inside3: "img/komiks/brygada/brygada_inside3.jpg",
+				inside4: "img/komiks/brygada/brygada_inside4.jpg",
+				},
+			availability: true,
+			filter: "Antologie/Integrale",
+			new: false			
+		}
 ];
+
+
+// Comics Brygada - Details
+const comicsBrygada = Comics[4];
+
+// Comics Brygada - Details - Title
+const titleBrygada = comicsBrygada.title;
+document.getElementById('title').innerHTML = titleBrygada;
+
+// Comics Brygada - Details - Writer
+const writerBrygada = comicsBrygada.writer;
+document.getElementById('writer').innerHTML = writerBrygada;
+
+// Comics Brygada - Details - Art
+const artBrygada = comicsBrygada.art;
+document.getElementById('art').innerHTML = artBrygada;
+
+// Comics Brygada - Details - Translator
+const translatorBrygada = comicsBrygada.translator;
+document.getElementById('translator').innerHTML = translatorBrygada;
+
+// Comics Brygada - Details - Proofreader
+const proofreaderBrygada = comicsBrygada.proofreader;
+document.getElementById('proofreader').innerHTML = proofreaderBrygada;
+
+// Comics Brygada - Details - Cover
+const coverBrygada = comicsBrygada.img.coverRegular;
+document.getElementById('cover-brygada').style.backgroundImage = `url(${coverBrygada}`;
+
+
+function chooseComics(number) {
+	
+}
+
+
+
+
+
+// Comics Metronom - Details
+const comicsMetronom = Comics[0];
+
+//const titleMetronom = comicsMetronom.title;
+//document.getElementById("title").innerHTML = titleMetronom;
+
+
 
 // Sort - Alphabetical Order - by Title
 const copyComicsA = Comics.slice();
@@ -118,7 +189,3 @@ console.log('OK');
 	return sortComicsByIdNewest
 
 });
-
-
-
-
