4807c7c4216f4b2630e9745ac59fc79a4df74e98
kajsujatu <michalmatwijow@gmail.com>
2022-09-21 23:13:56 +0200

    Working on main site, adding object values in js

---
+++ js/comics-site.js
@@ -0,0 +1,181 @@
+import {
+	Comics
+} from './script.js';
+
+
+// Choose number of Comics and fill divs with data
+function chooseComics(num) {
+	
+	// Title
+	const comicsTitle = Comics[num].title;
+	document.getElementById('title').innerHTML = comicsTitle;
+
+	// Writer
+	const comicsWriter = Comics[num].writer;
+	document.getElementById('writer').innerHTML = comicsWriter;		
+
+	// Art
+	const comicsArt = Comics[num].art;
+	document.getElementById('art').innerHTML = comicsArt;
+
+	// Translator
+	const comicsTranslator = Comics[num].translator;
+	document.getElementById('translator').innerHTML = comicsTranslator;
+
+	// Proofreader
+	const comicsProofreader = Comics[num].proofreader;
+	document.getElementById('proofreader').innerHTML = comicsProofreader;
+
+	// Cover - Main Site
+	// const comicsCover = Comics[num].img.coverRegular;
+	// document.getElementById('cover-regular').style.backgroundImage = `url(${comicsCover}`;
+
+	// Cover
+	const comicsCover = Comics[num].img.coverRegular;
+	document.getElementById('cover-big').src = `${comicsCover}`;
+
+	// Cover
+	const comicsCoverRegularThumbnail = Comics[num].img.coverRegular;
+	document.getElementById('cover-regular-thumbnail').src = `${comicsCoverRegularThumbnail}`;
+	document.getElementById('cover-regular-thumbnail').style.display = "inline";
+	document.getElementById('cover-regular-thumbnail').style.opacity = "50%";
+
+	// Cover Alternative 1
+	const comicsCoverAlt1 = Comics[num].img.coverAlternative1;
+	document.getElementById('cover-alternative1-thumbnail').src = `${comicsCoverAlt1}`;
+	document.getElementById('cover-alternative1-thumbnail').style.display = "inline";
+	if (comicsCoverAlt1 != null) {
+		document.getElementById('cover-alternative1-thumbnail').style.display = "inline";
+	} else {
+			document.getElementById('cover-alternative1-thumbnail').style.display = "none";
+	};
+
+	// Cover Alternative 2
+	const comicsCoverAlt2 = Comics[num].img.coverAlternative2;
+	document.getElementById('cover-alternative2-thumbnail').src = `${comicsCoverAlt2}`;
+	if (comicsCoverAlt2 != null) {
+	document.getElementById('cover-alternative2-thumbnail').style.display = "inline";
+	} else {
+		document.getElementById('cover-alternative2-thumbnail').style.display = "none";
+	};
+
+	// Functions for changing thumbnails
+	const coverRegularThumbnail = document.getElementById('cover-regular-thumbnail');
+	const coverBig = document.getElementById('cover-big');
+	const coverAlternative1Thumbnail = document.getElementById('cover-alternative1-thumbnail');
+	const coverAlternative2Thumbnail = document.getElementById('cover-alternative2-thumbnail');
+	
+	coverRegularThumbnail.addEventListener('click', function() {	
+		if (coverBig.src != Comics[num].img.coverRegular) {
+			coverBig.src = Comics[num].img.coverRegular;
+			coverRegularThumbnail.style.opacity = "50%";
+			coverAlternative1Thumbnail.style.opacity = "100%";
+		};
+	});
+	
+	coverAlternative1Thumbnail.addEventListener('click', function() {	
+		if (coverBig.src != Comics[num].img.coverAlternative1) {
+			coverBig.src = Comics[num].img.coverAlternative1;
+			coverAlternative1Thumbnail.style.opacity = "50%";
+			coverRegularThumbnail.style.opacity = "100%";
+			coverAlternative2Thumbnail.style.opacity = "100%";
+		};
+	});
+
+	coverAlternative2Thumbnail.addEventListener('click', function() {	
+		if (coverBig.src != Comics[num].img.coverAlternative2) {
+			coverBig.src = Comics[num].img.coverAlternative2;
+			coverAlternative2Thumbnail.style.opacity = "50%";
+			coverRegularThumbnail.style.opacity = "100%";
+			coverAlternative1Thumbnail.style.opacity = "100%";
+		};
+	});
+
+	// Inside 1
+	const comicsInside1 = Comics[num].img.inside1;
+	const comicsInside1Alt = Comics[num].img.inside1Alt;
+	document.getElementById('inside1-thumbnail').src = `${comicsInside1}`;
+	document.getElementById('inside1-thumbnail').setAttribute("alt",`${comicsInside1Alt}`);
+
+	// Inside 2
+	const comicsInside2 = Comics[num].img.inside2;
+	Comics[num].img.inside2
+	document.getElementById('inside2-thumbnail').src = `${comicsInside2}`;
+	
+	// Inside 3
+	const comicsInside3 = Comics[num].img.inside3;
+	Comics[num].img.inside3
+	document.getElementById('inside3-thumbnail').src = `${comicsInside3}`;
+
+	// Inside 4
+	const comicsInside4 = Comics[num].img.inside4;
+	Comics[num].img.inside4
+	document.getElementById('inside4-thumbnail').src = `${comicsInside4}`;
+
+	// Description Bold
+	const comicsDescriptionBold = Comics[num].description.bold;
+	document.getElementById('description-bold').innerHTML = comicsDescriptionBold;
+
+	// Description Normal
+	const comicsDescriptionNormal = Comics[num].description.normal;
+	document.getElementById('description-normal').innerHTML = comicsDescriptionNormal;
+
+	// Details Format
+	const comicsDetailsFormat = Comics[num].details.format;
+	document.getElementById('details-format').innerHTML = comicsDetailsFormat;
+
+	// Details Pages
+	const comicsDetailsPages = Comics[num].details.pages;
+	document.getElementById('details-pages').innerHTML = comicsDetailsPages;
+
+	// Details Print
+	const comicsDetailsPrint = Comics[num].details.print;
+	document.getElementById('details-print').innerHTML = comicsDetailsPrint;
+
+	// Details Paper
+	const comicsDetailsPaper = Comics[num].details.paper;
+	document.getElementById('details-paper').innerHTML = comicsDetailsPaper;
+
+	// Details Type of book cover
+	const comicsDetailsTypeOfBookCover = Comics[num].details.typeOfBookCover;
+	document.getElementById('details-type-of-book-cover').innerHTML = comicsDetailsTypeOfBookCover;
+
+	// Details Date
+	const comicsDetailsDate = Comics[num].details.date;
+	document.getElementById('details-date').innerHTML = comicsDetailsDate;
+
+	// Details Edition
+	const comicsDetailsEdition = Comics[num].details.edition;
+	document.getElementById('details-edition').innerHTML = comicsDetailsEdition;
+
+	// Details ISBN
+	const comicsDetailsISBN = Comics[num].details.ISBN;
+	document.getElementById('details-isbn').innerHTML = comicsDetailsISBN;
+
+	// Details Price
+	const comicsDetailsPrice = Comics[num].details.price;
+	document.getElementById('details-price').innerHTML = comicsDetailsPrice;
+
+	return;
+};
+
+chooseComics(4);
+
+
+function getPics() {}
+const imgs = document.querySelectorAll('.gallery-img');
+const fullPage = document.querySelector('#fullpage');
+
+imgs.forEach(img => {
+  img.addEventListener('click', function() {
+    fullPage.style.backgroundImage = 'url(' + img.src + ')';
+    fullPage.style.display = 'block';
+  });
+});
+
+const leftArrow = document.getElementById('left-arrow');
+const rightArrow = document.getElementById('right-arrow');
+
+leftArrow.addEventListener('click', function() {
+console.log("Działa");
+});
--- js/main-site.js
+++ js/main-site.js
@@ -1,41 +1,15 @@
-// Dark and light mode - variables
-const moon = document.getElementById('moon');
-const sun = document.getElementById('sun');
-const container = document.getElementById('container');
-const customizeText = document.querySelectorAll('.customize-text');
+//import {
+//	Comics
+//} from './script.js'
 
-// Dark and light mode - functions
-moon.addEventListener('click', function() {
-	document.body.style.backgroundColor = "#121212";
-	sun.style.display = "inline";
-	moon.style.display = "none";
-	container.style.backgroundColor = "#121212";
-	//customizeText.style.color = "white";
-});
 
-sun.addEventListener('click', function() {
-	document.body.style.backgroundColor = "#E7E9EC";
-	sun.style.display = "none";
-	moon.style.display = "inline";
-	container.style.backgroundColor = "#E7E9EC";
-});
-
-// Search button
-const search = document.getElementById('search');
-const searchInput = document.getElementById('search-input');
-
-search.addEventListener('click', function() {
-	searchInput.style.display = "inherit";
-	//searchInput.style.transform = "scaleX(2)";
-	//searchInput.style.backgroundColor: "#DCDCDC";
-});
 
 
 //Array of all Comics
-export const Comics = [
+const Comics = [
 	// Metronom
 		{
-			id: 2, 
+			id: 1, 
 			title: "Metronom'",
 			writer: "Éric Corbeyran",
 			art: "Grun",
@@ -77,7 +51,7 @@ export const Comics = [
 				date: "2010-2015"
 				},
 			filter: "Antologie/Integrale",
-			new: false,		
+			new: true,		
 			availability: true
 		},	
 	// Wiek Evy - XXe ciel.com
@@ -158,9 +132,95 @@ export const Comics = [
 				},
 			filter: "Antologie/Integrale",
 			new: false,		
-			availability: true
+			availability: true,
+			url: "../komiks/brygada.html"
 		}
 ];
+
+
+
+
+
+
+
+// Cover - Main Site
+const comicsCoverMain0 = Comics[0].img.coverMainSite;
+document.getElementById('cover-metronom').style.backgroundImage = `url(${comicsCoverMain0}`;
+const comicsOrder0 = Comics[0].id;
+document.getElementById('cover-metronom').style.order = comicsOrder0;
+
+if (Comics[0].new != false) {
+	document.getElementById('cover-metronom').classList.add("new");
+};
+
+const comicsCoverMain4 = Comics[4].img.coverMainSite;
+document.getElementById('cover-brygada').style.backgroundImage = `url(${comicsCoverMain4}`;
+
+const comicsOrder4 = Comics[4].id;
+document.getElementById('cover-brygada').style.order = comicsOrder4;
+
+if (Comics[4].new != false) {
+	document.getElementById('cover-brygada').classList.add("new");
+} 
+
+const coverBrygada = document.getElementById('cover-brygada');
+coverBrygada.addEventListener("click", function(){
+	window.location = "../komiks/brygada.html";
+});
+
+//document.getElementById('cover-brygada').setAttribute("href", comicsLink);
+
+
+
+
+
+
+
+
+
+
+// Sort - Alphabetical Order - by Title
+const copyComicsA = Comics.slice();
+
+const sortAlphabetical = copyComicsA.sort((a, b) => {
+	const titleA = a.title.toUpperCase();
+	const titleB = b.title.toUpperCase();
+	if (titleA < titleB) {
+	  return -1;
+	}
+	if (titleA > titleB) {
+	  return 1;
+	}
+  	return 0;
+  });
+
+// Sort - Newest - by Id - Descending
+//const copyComicsN = Comics.slice();
+
+//const sortComicsByIdNewest = copyComicsN.sort((a, b) => b.id - a.id);
+
+// Sort - Oldest - by Id - Ascending
+const copyComicsO = Comics.slice();
+
+const sortComicsByIdOldest = copyComicsO.sort((a, b) => a.id - b.id);
+
+
+const sortInput = document.getElementById('sort-input');
+
+const sortNewest = sortInput.options['newest'];
+
+sortNewest.addEventListener('click', function() {
+
+	const copyComicsN = Comics.slice();
+	const sortComicsByIdNewest = copyComicsN.sort((a, b) => b.id - a.id);
+
+console.log('OK');
+
+	return sortComicsByIdNewest
+
+});
+
+
 
 
 
